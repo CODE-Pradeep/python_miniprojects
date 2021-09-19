9923914740117ae78fdec8b390952e57be45c8f6
Pradeep Kumar Sharma <76009650+CODE-Pradeep@users.noreply.github.com>
2021-09-20 00:45:07 +0530

    Add files via upload

---
+++ aktupresentage.py
@@ -0,0 +1,15 @@
+a = int(input("enter your 1st year marks: "))
+b = int(input("enter your 2nd year marks: "))
+c = int(input("enter your 3rd year marks: "))
+d = int(input("enter your 4th year marks: "))
+
+
+
+first =a*25/100
+sec =b*50/100
+third =c*75/100
+fourth =d*100/100
+
+sum=first+sec+third+fourth
+result=sum/4950*100
+print("your b tech presnetage is :",result)
